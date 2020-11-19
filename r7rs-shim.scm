5abe7063665191bedb5d4781e952580bbe42c2b6
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2020-11-19 15:19:35 -0500

    Add real version of floor-remainder.

--- r7rs-shim.scm
+++ r7rs-shim.scm
@@ -2,7 +2,21 @@
   (let ((eof (read (open-input-string ""))))
     (lambda () eof)))
 
-(define floor-remainder remainder)
+;; Snarfed from the r7rs egg.  Why didn't this get added to
+;; (chicken base) in CHICKEN 5?
+(define (floor/ x y)
+  (let-values (((div rem) (quotient&remainder x y)))
+    (if (positive? y)
+        (if (negative? rem)
+            (values (- div 1) (+ rem y))
+            (values div rem))
+        (if (positive? rem)
+            (values (- div 1) (+ rem y))
+            (values div rem)))))
+
+(define (floor-remainder x y)
+  (let-values (((_ rem) (floor/ x y)))
+    rem))
 
 ;; Single-vector-only version.
 (define (vector-map f vec)
