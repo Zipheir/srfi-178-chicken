237da1fe7f27a9c7e248f784cf1291da8fd3a5c7
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-08-17 11:28:13 -0400

    Types and type-checks for r7rs-shim.

--- r7rs-shim.scm
+++ r7rs-shim.scm
@@ -1,7 +1,11 @@
+(: eof-object (-> eof))
 (define (eof-object) #!eof)
 
 ;; Single-vector-only version.
+(: vector-map ((* -> *) vector -> vector))
 (define (vector-map f vec)
+  (assert-type 'vector-map (procedure? f))
+  (assert-type 'vector-map (vector? vec))
   (let* ((len (vector-length vec))
          (res (make-vector len)))
     (let lp ((i 0))
