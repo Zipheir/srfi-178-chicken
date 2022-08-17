8b91a8d07ce255c7cec0fdca5c8d1a26eeef3ba3
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-08-17 12:16:53 -0400

    %bit?: Return #f for all non-bit arguments.
    Previously, this internal predicate signaled an error on some
    arguments.

--- 178/util.scm
+++ 178/util.scm
@@ -1,4 +1,7 @@
 (define (exact-natural? x)
   (and (exact-integer? x) (not (negative? x))))
 
-(define (%bit? x) (or (boolean? x) (zero? x) (= x 1)))
+(define (%bit? x)
+  (cond ((boolean? x) #t)
+        ((integer? x) (or (zero? x) (= x 1)))
+        (else #f)))
