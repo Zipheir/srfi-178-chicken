33e35c7f12d032a11792d4b14b8d9a4d4cfef8f1
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-08-19 22:31:56 -0400

    Field ops: Add range checks.

--- 178/fields.scm
+++ 178/fields.scm
@@ -3,6 +3,7 @@
   (assert-type 'bitvector-field-any? (bitvector? bvec))
   (assert-type 'bitvector-field-any? (exact-natural? start))
   (assert-type 'bitvector-field-any? (exact-natural? end))
+  (%check-range 'bitvector-field-any? bvec start end)
   (let lp ((i start))
     (and (< i end)
          (or (bitvector-ref/bool bvec i)
@@ -13,6 +14,7 @@
   (assert-type 'bitvector-field-every? (bitvector? bvec))
   (assert-type 'bitvector-field-every? (exact-natural? start))
   (assert-type 'bitvector-field-every? (exact-natural? end))
+  (%check-range 'bitvector-field-every? bvec start end)
   (let lp ((i start))
     (or (>= i end)
         (and (bitvector-ref/bool bvec i)
@@ -31,6 +33,7 @@
   (assert-type 'bitvector-field-clear (bitvector? bvec))
   (assert-type 'bitvector-field-clear (exact-natural? start))
   (assert-type 'bitvector-field-clear (exact-natural? end))
+  (%check-range 'bitvector-field-clear bvec start end)
   (%bitvector-field-modify bvec 0 start end))
 
 (: %bitvector-fill!/int (bitvector integer integer integer -> undefined))
@@ -42,6 +45,7 @@
   (assert-type 'bitvector-field-clear! (bitvector? bvec))
   (assert-type 'bitvector-field-clear! (exact-natural? start))
   (assert-type 'bitvector-field-clear! (exact-natural? end))
+  (%check-range 'bitvector-field-clear! bvec start end)
   (%bitvector-fill!/int bvec 0 start end))
 
 (: bitvector-field-set (bitvector integer integer -> bitvector))
@@ -49,6 +53,7 @@
   (assert-type 'bitvector-field-set (bitvector? bvec))
   (assert-type 'bitvector-field-set (exact-natural? start))
   (assert-type 'bitvector-field-set (exact-natural? end))
+  (%check-range 'bitvector-field-set bvec start end)
   (%bitvector-field-modify bvec 1 start end))
 
 (: bitvector-field-set! (bitvector integer integer -> undefined))
@@ -56,6 +61,7 @@
   (assert-type 'bitvector-field-set! (bitvector? bvec))
   (assert-type 'bitvector-field-set! (exact-natural? start))
   (assert-type 'bitvector-field-set! (exact-natural? end))
+  (%check-range 'bitvector-field-set! bvec start end)
   (%bitvector-fill!/int bvec 1 start end))
 
 (: bitvector-field-replace
@@ -65,6 +71,7 @@
   (assert-type 'bitvector-field-replace (bitvector? source))
   (assert-type 'bitvector-field-replace (exact-natural? start))
   (assert-type 'bitvector-field-replace (exact-natural? end))
+  (%check-range 'bitvector-field-replace bvec start end)
   (bitvector-unfold
    (lambda (i)
      (if (and (>= i start) (< i end))
@@ -79,6 +86,7 @@
   (assert-type 'bitvector-field-replace! (bitvector? source))
   (assert-type 'bitvector-field-replace! (exact-natural? start))
   (assert-type 'bitvector-field-replace! (exact-natural? end))
+  (%check-range 'bitvector-field-replace! bvec start end)
   (bitvector-copy! dest start source 0 (- end start)))
 
 (: bitvector-field-replace-same
@@ -88,6 +96,7 @@
   (assert-type 'bitvector-field-replace-same (bitvector? source))
   (assert-type 'bitvector-field-replace-same (exact-natural? start))
   (assert-type 'bitvector-field-replace-same (exact-natural? end))
+  (%check-range 'bitvector-field-replace-same bvec start end)
   (bitvector-unfold
    (lambda (i)
      (bitvector-ref/int (if (and (>= i start) (< i end))
@@ -103,6 +112,7 @@
   (assert-type 'bitvector-field-replace-same! (bitvector? source))
   (assert-type 'bitvector-field-replace-same! (exact-natural? start))
   (assert-type 'bitvector-field-replace-same! (exact-natural? end))
+  (%check-range 'bitvector-field-replace-same! bvec start end)
   (bitvector-copy! dest start source start end))
 
 (: bitvector-field-rotate (bitvector integer integer integer -> bitvector))
@@ -111,6 +121,7 @@
   (assert-type 'bitvector-field-rotate (exact-integer? count))
   (assert-type 'bitvector-field-rotate (exact-natural? start))
   (assert-type 'bitvector-field-rotate (exact-natural? end))
+  (%check-range 'bitvector-field-rotate bvec start end)
   (if (zero? count)
       bvec
       (let ((field-len (- end start)))
@@ -128,6 +139,7 @@
   (assert-type 'bitvector-field-flip (bitvector? bvec))
   (assert-type 'bitvector-field-flip (exact-natural? start))
   (assert-type 'bitvector-field-flip (exact-natural? end))
+  (%check-range 'bitvector-field-flip bvec start end)
   (bitvector-unfold
    (lambda (i)
      (I (if (and (>= i start) (< i end))
@@ -140,6 +152,7 @@
   (assert-type 'bitvector-field-flip! (bitvector? bvec))
   (assert-type 'bitvector-field-flip! (exact-natural? start))
   (assert-type 'bitvector-field-flip! (exact-natural? end))
+  (%check-range 'bitvector-field-flip! bvec start end)
   (let lp ((i start))
     (unless (>= i end)
       (bitvector-set! bvec i (not (bitvector-ref/bool bvec i)))
